c7a6b12f76da0fa093c2b22354828f0247243336
aryan7412 <aryan7412ss@gmail.com>
2025-10-18 01:24:26 +0530

    Intial Setup NextJS

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ModeToggle} from "@/components/ModeToggle"
+
+const page = () => {
+  return (
+    <div className="h-screen items-center justify-center flex">
+      <ModeToggle />
+    </div>
+  )
+}
+
+export default page
